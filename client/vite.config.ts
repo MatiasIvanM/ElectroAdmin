f52c5034e58e8eac847087961731635de7185fd2
MatiasIvanM <matiasivan.sd@gmail.com>
2025-11-14 00:39:54 -0300

    Vite compìler update

---
+++ client/vite.config.ts
@@ -0,0 +1,14 @@
+import { defineConfig } from 'vite';
+import path from 'path';
+
+export default defineConfig(async () => {
+  const reactPlugin = (await import('@vitejs/plugin-react')).default;
+  return {
+    plugins: [reactPlugin()],
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, 'src'),
+      },
+    },
+  };
+});
